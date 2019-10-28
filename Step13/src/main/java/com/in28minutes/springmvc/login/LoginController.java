0390938b1fe38d16d20a83bdb1f3136fe4206e4e
Ranga Karanam <rangakaranam@Rangas-MacBook-Air.local>
2019-10-28 14:34:27 +0530

    Thank You for Choosing to Learn from in28Minutes

---
+++ Step13/src/main/java/com/in28minutes/springmvc/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springmvc.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
+		return "login";
+	}
+}
---
+++ Step13/src/main/webapp/WEB-INF/views/welcome.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Yahoo!!</title>
+</head>
+<body>
+Welcome ${name}
+</body>
+</html>
